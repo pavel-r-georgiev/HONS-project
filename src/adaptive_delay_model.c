5392b9e7321f5e2fe2c3acaedfb9d356603dffe1
Pavel Georgiev <pavel.r.georgiev@hotmail.com>
2019-01-03 21:22:52 +0000

    Change adaptive failure detection from Bertier algorithm to 2 window algorithm

--- src/adaptive_delay_model.c
+++ src/adaptive_delay_model.c
@@ -12,45 +12,66 @@
 adaptive_timeout_struct* init_adaptive_timeout_struct(double first_heartbeat_ms) {
     adaptive_timeout_struct *metadata_struct  = (struct adaptive_timeout_struct *)malloc(sizeof *metadata_struct);
     metadata_struct->first_heartbeat_arrival_ms = first_heartbeat_ms;
-    metadata_struct->freshness_delta = 0;
     metadata_struct->freshness_point = 0;
     metadata_struct-> arrival_time_ms = 0;
     metadata_struct ->estimated_arrival_ms = 0;
-    metadata_struct -> u = 0;
     metadata_struct ->delay = INITIAL_DELAY;
     metadata_struct ->alpha = 0;
     metadata_struct -> var = 0;
     metadata_struct -> error = 0;
     metadata_struct -> seq_num = -1;
-
+    metadata_struct -> sum_w1 = 0L;
+    metadata_struct->past_arrival_time_differences_w1_ms = malloc(W1_WINDOW_SIZE* sizeof(double));
+    metadata_struct->past_arrival_time_differences_w2_ms = malloc(W2_WINDOW_SIZE* sizeof(double));
     return metadata_struct;
 }
 
-void estimate_next_delay(adaptive_timeout_struct *timeout_model, double current_arrival_time) {
+void estimate_next_delay(adaptive_timeout_struct *timeout_model, double current_arrival_time, FILE *fp) {
+    double estimated_arrival_w1, estimated_arrival_w2;
     timeout_model->seq_num++;
     timeout_model->arrival_time_ms = current_arrival_time - timeout_model->first_heartbeat_arrival_ms;
     long k = timeout_model->seq_num;
     double t = timeout_model->arrival_time_ms;
 
-    timeout_model->error = t - timeout_model->estimated_arrival_ms - timeout_model->alpha;
+//    Compute alpha using Jacobson algorithm variables
+    timeout_model->error = t - timeout_model->estimated_arrival_ms - timeout_model->delay;
     timeout_model->delay = timeout_model->delay + GAMMA*timeout_model->error;
     timeout_model-> var = timeout_model->var + GAMMA*(fabs(timeout_model->error) - timeout_model->var);
     timeout_model->alpha = BETA*timeout_model->delay + PHI*timeout_model->var;
 
-    if(k <= MESSAGE_COUNT_FOR_DATA_AGGREGATION){
-        timeout_model->u = (t/(k+1)) * ((k*timeout_model->u)/(k+1));
-        timeout_model->estimated_arrival_ms = timeout_model->u + ((k+1)/2.0)*(double)HEARTBEAT_INTERVAL;
+//    Calculate sum within sliding window w1 depending on sequence number
+    if(k < W1_WINDOW_SIZE){
+        timeout_model->sum_w1 += t - k*HEARTBEAT_INTERVAL;
     } else {
-        long past_time_index = (k - MESSAGE_COUNT_FOR_DATA_AGGREGATION - 1) % MESSAGE_COUNT_FOR_DATA_AGGREGATION;
-        double past_time =  timeout_model->past_arrival_times_ms[past_time_index];
-        timeout_model->estimated_arrival_ms = timeout_model->estimated_arrival_ms + (1.0/k)*(t - past_time);
+        long past_time_index = (k - W1_WINDOW_SIZE) % W1_WINDOW_SIZE;
+        double past_time_difference =  timeout_model->past_arrival_time_differences_w1_ms[past_time_index];
+        timeout_model->sum_w1 -= past_time_difference;
+        timeout_model->sum_w1 += t - k*HEARTBEAT_INTERVAL;
+    }
+//    Calculate sum within sliding window w2 depending on sequence number
+    if(k < W2_WINDOW_SIZE){
+        timeout_model->sum_w2 += t - k*HEARTBEAT_INTERVAL;
+    } else {
+        long past_time_index = (k - W2_WINDOW_SIZE) % W2_WINDOW_SIZE;
+        double past_time_difference =  timeout_model->past_arrival_time_differences_w2_ms[past_time_index];
+        timeout_model->sum_w2 -= past_time_difference;
+        timeout_model->sum_w2 += t - k*HEARTBEAT_INTERVAL;
     }
 
-    timeout_model->past_arrival_times_ms[k % MESSAGE_COUNT_FOR_DATA_AGGREGATION] = t;
-    timeout_model->freshness_point = timeout_model->freshness_point + timeout_model->estimated_arrival_ms + timeout_model->alpha;
+    if(k == 0){
+        timeout_model->estimated_arrival_ms = HEARTBEAT_INTERVAL;
+    } else {
+        estimated_arrival_w1 = (1.0/k)*timeout_model->sum_w1 + (k+1)*HEARTBEAT_INTERVAL;
+        estimated_arrival_w2 = (1.0/k)*timeout_model->sum_w2 + (k+1)*HEARTBEAT_INTERVAL;
+        timeout_model->estimated_arrival_ms =  (estimated_arrival_w1 > estimated_arrival_w2 ) ? estimated_arrival_w1 : estimated_arrival_w2;
+    }
+
+    timeout_model->past_arrival_time_differences_w1_ms[k % W1_WINDOW_SIZE] = t - k*HEARTBEAT_INTERVAL;
+    timeout_model->freshness_point = timeout_model->estimated_arrival_ms + timeout_model->alpha;
 
     printf("%f %f %f \n", timeout_model->arrival_time_ms, timeout_model->freshness_point, timeout_model->estimated_arrival_ms);
-//    IF NODE IS SUSPECT --> REMOVE FROM SUSPECTS LIST
+    fprintf(fp,"%f,%f,%f \n", timeout_model->arrival_time_ms, timeout_model->freshness_point, timeout_model->estimated_arrival_ms);
+//    TODO: IF NODE IS SUSPECT --> MOVE TO SUSPECTS LIST AND HANDLE REMOVAL FROM MEMBERSHIP
     if(0){
         timeout_model->freshness_delta++;
     }
--- src/adaptive_delay_model.h
+++ src/adaptive_delay_model.h
@@ -1,9 +1,11 @@
 
 #ifndef HONS_PROJECT_ADAPTIVE_DELAY_MODEL_H
 #define HONS_PROJECT_ADAPTIVE_DELAY_MODEL_H
+#include<stdio.h>
 
-#define INITIAL_DELAY 0
-#define MESSAGE_COUNT_FOR_DATA_AGGREGATION 25
+#define INITIAL_DELAY 5000
+#define W1_WINDOW_SIZE 5
+#define W2_WINDOW_SIZE 1000
 #define HEARTBEAT_INTERVAL 1000
 
 typedef struct adaptive_timeout_struct {
@@ -11,14 +13,17 @@ typedef struct adaptive_timeout_struct {
     double freshness_point;
     double arrival_time_ms;
     double first_heartbeat_arrival_ms;
-    double past_arrival_times_ms[MESSAGE_COUNT_FOR_DATA_AGGREGATION];
+    double* past_arrival_time_differences_w1_ms;
+    double* past_arrival_time_differences_w2_ms;
+    double* time_between_messages;
     double estimated_arrival_ms;
-    double u;
     double delay;
     double alpha;
     double var;
     double error;
     long seq_num;
+    double sum_w1;
+    double sum_w2;
 } adaptive_timeout_struct;
 
 adaptive_timeout_struct* init_adaptive_timeout_struct(double first_heartbeat_ms);
