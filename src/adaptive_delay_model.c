604710aab84d84f4b2feb60739279bc77ec54280
Pavel Georgiev <pavel.r.georgiev@hotmail.com>
2019-01-04 20:56:58 +0000

    Fix adaptive delay model when limit samples in window

--- src/adaptive_delay_model.c
+++ src/adaptive_delay_model.c
@@ -48,7 +48,7 @@ void estimate_next_delay(adaptive_timeout_struct *timeout_model, double current_
     if(k < W1_WINDOW_SIZE){
         timeout_model->sum_w1 += t - k*HEARTBEAT_INTERVAL;
     } else {
-        long past_time_index = (k - W1_WINDOW_SIZE) % W1_WINDOW_SIZE;
+        long past_time_index = k % W1_WINDOW_SIZE;
         double past_time_difference =  timeout_model->past_arrival_time_differences_w1_ms[past_time_index];
         timeout_model->sum_w1 -= past_time_difference;
         timeout_model->sum_w1 += t - k*HEARTBEAT_INTERVAL;
@@ -58,31 +58,32 @@ void estimate_next_delay(adaptive_timeout_struct *timeout_model, double current_
         timeout_model->sum_w2 += t - k*HEARTBEAT_INTERVAL;
         timeout_model->sum_time_deltas += time_from_last_heartbeat;
     } else {
-        long past_time_index = (k - W2_WINDOW_SIZE) % W2_WINDOW_SIZE;
+        long past_time_index = k % W2_WINDOW_SIZE;
         double past_time_difference =  timeout_model->past_arrival_time_differences_w2_ms[past_time_index];
 
         timeout_model->sum_w2 -= past_time_difference;
         timeout_model->sum_w2 += t - k*HEARTBEAT_INTERVAL;
-
         timeout_model->sum_time_deltas -= timeout_model->deltas_between_messages[past_time_index];
         timeout_model->sum_time_deltas += time_from_last_heartbeat;
     }
 
-    average_heartbeat_time = timeout_model->sum_time_deltas / k;
-
     if(k == 0){
         timeout_model->estimated_arrival_ms = HEARTBEAT_INTERVAL;
+        average_heartbeat_time = HEARTBEAT_INTERVAL;
     } else {
+//        Number of samples in bigger window is currently min(k, w2 window size)
+        long number_of_samples_currently_in_w2 = k < W2_WINDOW_SIZE ? k : W2_WINDOW_SIZE;
+        average_heartbeat_time = timeout_model->sum_time_deltas / number_of_samples_currently_in_w2;
         estimated_arrival_w1 = (1.0/k)*timeout_model->sum_w1 + (k+1)*average_heartbeat_time;
         estimated_arrival_w2 = (1.0/k)*timeout_model->sum_w2 + (k+1)*average_heartbeat_time;
         timeout_model->estimated_arrival_ms =  (estimated_arrival_w1 > estimated_arrival_w2 ) ? estimated_arrival_w1 : estimated_arrival_w2;
     }
 
     timeout_model->deltas_between_messages[k % W2_WINDOW_SIZE] = time_from_last_heartbeat;
-    timeout_model->past_arrival_time_differences_w1_ms[k % W1_WINDOW_SIZE] = t - k*HEARTBEAT_INTERVAL;
-    timeout_model->past_arrival_time_differences_w2_ms[k % W2_WINDOW_SIZE] = t - k*HEARTBEAT_INTERVAL;
+    timeout_model->past_arrival_time_differences_w1_ms[k % W1_WINDOW_SIZE] = t - k*average_heartbeat_time;
+    timeout_model->past_arrival_time_differences_w2_ms[k % W2_WINDOW_SIZE] = t - k*average_heartbeat_time;
     timeout_model->freshness_point = timeout_model->estimated_arrival_ms + timeout_model->alpha;
-    timeout_model->next_timeout = timeout_model->freshness_point - current_arrival_time;
+    timeout_model->next_timeout = timeout_model->freshness_point - timeout_model->arrival_time_ms;
 
     if(DEBUG){
         printf("A:%f EA:%f FP:%f Timeout:%f \n", timeout_model->arrival_time_ms,  timeout_model-> estimated_arrival_ms, timeout_model->freshness_point, timeout_model->next_timeout);
